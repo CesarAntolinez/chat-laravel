af70d5515f36d76bb331899baface5b355a07271
antolinez <cesaralejandroantolinez@gmail.com>
2023-01-23 21:59:41 -0500

    javascript

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,69 +1,40 @@
-<!doctype html>
-<html lang="es">
+<!DOCTYPE html>
+<html lang="en" >
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>CodePen - Simple Chat UI</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="{{ asset('css/chat.css') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/chat.css">
 </head>
 <body>
 <section class="msger">
     <header class="msger-header">
         <div class="msger-header-title">
-            <i class="fas fa-comment-alt"></i> SimpleChat
+            <i class="fas fa-comment-alt"></i>
+            <span class="chatWith"></span>
+            <span class="typing" style="display: none;"> está escribiendo</span>
         </div>
         <div class="msger-header-options">
-            <span><i class="fas fa-cog"></i></span>
+            <span class="chatStatus offline"><i class="fas fa-globe"></i></span>
         </div>
     </header>
 
-    <main class="msger-chat">
-        <div class="msg left-msg">
-            <div
-                class="msg-img"
-                style="background-image: url(https://image.flaticon.com/icons/svg/327/327779.svg)"
-            ></div>
-
-            <div class="msg-bubble">
-                <div class="msg-info">
-                    <div class="msg-info-name">BOT</div>
-                    <div class="msg-info-time">12:45</div>
-                </div>
-
-                <div class="msg-text">
-                    Hi, welcome to SimpleChat! Go ahead and send me a message. 😄
-                </div>
-            </div>
-        </div>
-
-        <div class="msg right-msg">
-            <div
-                class="msg-img"
-                style="background-image: url(https://image.flaticon.com/icons/svg/145/145867.svg)"
-            ></div>
-
-            <div class="msg-bubble">
-                <div class="msg-info">
-                    <div class="msg-info-name">Sajad</div>
-                    <div class="msg-info-time">12:46</div>
-                </div>
-
-                <div class="msg-text">
-                    You can change your name in JS section!
-                </div>
-            </div>
-        </div>
-    </main>
+    <div class="msger-chat"></div>
 
     <form class="msger-inputarea">
-        <input type="text" class="msger-input" placeholder="Enter your message...">
+        <input type="text" class="msger-input" oninput="sendTypingEvent()" placeholder="Enter your message...">
         <button type="submit" class="msger-send-btn">Send</button>
     </form>
+
 </section>
 
-<script src="{{ asset('js/chat.js') }}"></script>
+<script src='https://use.fontawesome.com/releases/v5.0.13/js/all.js'></script>
+<script  src="/js/app.js"></script>
+<script  src="/js/chat.js"></script>
+
 </body>
 </html>
